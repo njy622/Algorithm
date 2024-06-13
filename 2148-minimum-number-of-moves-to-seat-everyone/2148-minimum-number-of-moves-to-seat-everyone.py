e9913e77cef4585ee029776f30dd6c05ed99beab
Ji-yeong Nam <139431528+njy622@users.noreply.github.com>
2024-06-13 21:14:23 +0900

    Time: 55 ms (81.76%), Space: 16.5 MB (82.54%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.py
@@ -1,15 +1,11 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats = sorted(seats)
+        students = sorted(students)
+        
         count = 0
-        seats.sort()
-        students.sort()
-
+        
         for i in range(len(seats)):
-            if seats[i] > students[i]:
-                count += seats[i] - students[i]
-            else:
-                count += students[i] - seats[i]
-
+            count += abs(seats[i] - students[i])
+        
         return count
-
-                    
